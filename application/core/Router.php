707aadae92fd9a22bfa3c41b04ddf21c8871938e
Denis <den-sidnay@yandex.ru>
2019-12-01 22:28:28 +0300

    modif class route

--- application/core/Router.php
+++ application/core/Router.php
@@ -9,22 +9,43 @@ class Router
 	
 	public function __construct()
 	{
-		//
+		$routes = require 'application/config/routes.php';
+		foreach ($routes as $key => $value) {
+			$this->add($key, $value);
+		}
 	}
     
-    public function add()
+    public function add($route, $params)
     {
-    	//
+    	$route = '#^' . $route . '$#';
+    	$this->routes[$route] = $params;
     }
 
     public function match()
     {
-    	//
+    	$url = trim($_SERVER['REQUEST_URI'], '/');
+    	foreach ($this->routes as $route => $params) {
+    		if (preg_match($route, $url, $matches)){
+                  $this->params = $params;
+                  return true;
+    		}
+    	}
+    	return false;
     }
 
     public function run()
     {
-    	echo "start";
+    	if ($this->match()){
+             $controller = 'application\controller\\' . ucfirst($this->params['controller']) . 'Controller.php';
+             if (class_exists($controller)){
+             	echo "ok";
+             } else {
+             	echo $controller . " not found";
+             }
+
+    	} else {
+    		echo "NO";
+    	}
     }    
 
 
